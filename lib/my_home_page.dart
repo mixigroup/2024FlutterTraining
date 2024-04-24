6768c0956697f86f0ef6810920c67c2658c57312
久野文菜 <ayana.kuno@mixi.co.jp>
2024-04-24 16:43:23 +0900

    Github リポジトリ一覧取得

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 // こちらが　MyHomePage
 // StatefulWidget に関しても後で説明するよ！！！！！
@@ -13,12 +14,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  // リポジトリ取得するメソッド
+  // async キーワードは関数が非同期であることを示す
+  // async を付けた関数は内部で await を使用することができる＆常に Future オブジェクトを返す
+  Future<void> getRepositoryList() async {
+    // url とパスを書く
+    final url = Uri.https('api.github.com', 'users/kno3a87/repos');
+    // 今回は get でリポジトリ一覧取得！結果が response に入ってくる！
+    // ネットワーク通信は非同期処理なので await で待ってあげる
+    final response = await http.get(
+      url,
+      // ヘッダー書きたいならこう！
+      // ```
+      // headers: {
+      //   'Authorization':
+      //       'Bearer <MY-TOKEN>',
+      //   'X-GitHub-Api-Version': '2022-11-28',
+      //   'Accept': 'application/vnd.github+json'
+      // },
+      // ```
+    );
+    // ステータスコードを確認してみる
+    // 200OK なら成功！
+    debugPrint('Response status: ${response.statusCode}');
+    // ボディを確認してみる
+    // さっき curl で叩いた時と一緒のものが返ってきてるはず
+    debugPrint('Response body: ${response.body}');
   }
 
   @override
@@ -31,27 +52,23 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       // Center で真ん中寄せ
-      body: Center(
+      body: const Center(
         // Column は [] の中身を縦に並べてくれる widget
         // Row で横になるよ
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'You have pushed the button this many times:',
             ),
           ],
         ),
       ),
       // 右下のプラスボタン（Floating Action Button と言います）
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: getRepositoryList,
+        tooltip: 'get my repository list',
+        child: const Icon(Icons.star),
       ),
     );
   }
