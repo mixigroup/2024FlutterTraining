dde7e02db0f0729393a5f32b1e4638ec80ad5d64
久野文菜 <ayana.kuno@mixi.co.jp>
2024-04-24 12:27:10 +0900

    main.dart コメント追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,44 @@
 import 'package:flutter/material.dart';
 
+// 中枢！main.dart の main() が最初に呼ばれる
 void main() {
+  // 下の MyApp を run するよ〜
   runApp(const MyApp());
 }
 
+// こちらが MyApp
+// Widget を使うよってことで Widget を extend したクラスを作る
+// StatelessWidget に関しては後で説明するよ！
 class MyApp extends StatelessWidget {
+  // コンストラクタ
+  // クラスが作られたときにクラス内で使う変数を初期化するためのもの
+  // 今回は変数がないのでデフォルトの Key のみ突っ込まれてる
+  // Key, super の説明は今回は省略
   const MyApp({super.key});
 
+  // MaterialApp を作って返して表示させるよ！
+  // MaterialApp は Flutter アプリケーションの全体を管理するもので，全体のデザイン `theme: ` や
+  // 画面遷移をする場合の状態監視，最初に表示させるページ `home: ` を指定しているよ
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      // OS のテーマ設定に合わせて変更できる
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      // 最初に表示させるページをは下の MyHomePage
+      // 引数として title 渡してる（無くてもいいよ）
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+// こちらが　MyHomePage
+// StatefulWidget に関しても後で説明するよ！！！！！
 class MyHomePage extends StatefulWidget {
+  // title を受け取ってるね👀
   const MyHomePage({super.key, required this.title});
 
   final String title;
@@ -40,12 +58,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // Scaffold は土台みたいな感じ（白紙みたいな）
     return Scaffold(
+      // AppBar は上のヘッダー
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
+      // Center で真ん中寄せ
       body: Center(
+        // Column は [] の中身を縦に並べてくれる widget
+        // Row で横になるよ
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -59,6 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      // 右下のプラスボタン（Floating Action Button と言います）
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
