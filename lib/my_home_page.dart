ca6342c965d8a455c979d29164d173c21c1632bc
久野文菜 <ayana.kuno@mixi.co.jp>
2024-04-24 13:57:24 +0900

    2倍にする FAB の追加

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -14,10 +14,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _counter2 = 1;
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+    });
+  }
+
+  void _twice() {
+    setState(() {
+      _counter2 = _counter2 * 2;
     });
   }
 
@@ -38,20 +45,48 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              '足し算',
             ),
             Text(
               '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const Text(
+              '２倍されてく',
+            ),
+            Text(
+              '$_counter2',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       // 右下のプラスボタン（Floating Action Button と言います）
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Row(
+        // Row は親サイズまで広がるんだったね！
+        // min を指定して自分自身のサイズに合わせるよ
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 8),
+          FloatingActionButton(
+            // onPressed の書き方はお好みで😘
+            // 一行の関数や式を簡潔に記述する場合に便利なアロー関数記法
+            // onPressed: () => _twice(),
+            // 複数の命令や条件分岐をする場合や print デバッグとか挟んだりする時に便利なブロックボディ記法
+            // onPressed: () {
+            //   _twice();
+            // },
+            // 今回は簡潔に，メソッド名だけを指定して Flutter にメソッドの実行を委ねるダイレクトリファレンス記法で！
+            onPressed: _twice,
+            tooltip: 'Twice',
+            child: const Icon(Icons.close),
+          ),
+        ],
       ),
     );
   }
